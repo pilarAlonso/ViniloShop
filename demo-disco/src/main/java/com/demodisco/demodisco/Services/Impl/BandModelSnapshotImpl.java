1f0d738988fd70d53610bac3c72d9f67c8bbec8e
unknown <pilaralonsosuela@gmail.com>
2019-05-26 11:43:21 +0200

    correccion de lanzamiento de excepcion en bandsnapshotmodel

--- demo-disco/src/main/java/com/demodisco/demodisco/Services/Impl/BandModelSnapshotImpl.java
+++ demo-disco/src/main/java/com/demodisco/demodisco/Services/Impl/BandModelSnapshotImpl.java
@@ -26,9 +26,13 @@ public class BandModelSnapshotImpl implements BandModelSnapshotService {
 
 		if (lista.isEmpty()) throw new NotFound();
 
+
+
 		Comparator<Band> comparador1 = (x, y) -> Integer.compare(y.mostSold().size(), x.mostSold().size());
+
 		bandModelSnapshot.setFiveMostSoldGroup(lista.stream().sorted(comparador1)
 													.limit(5).collect(Collectors.toList()));
+		if(bandModelSnapshot.getFiveMostSoldGroup().size()<5)throw new NotFound();
 
 		return bandModelSnapshot;
 	}
